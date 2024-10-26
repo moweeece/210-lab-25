d5ee43a84b4eb78584eac02b883f67c3ba2ae1dd
moweeece <mjespinosa990@gmail.com>
2024-10-26 12:45:40 -0700

    Third commit

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 
 #include <iostream>
 #include <chrono>
-#include <vector>
-#include <list>
-#include <set>
-#include <fstream>
-#include <iomanip>
+#include <vector>    // for vector
+#include <list>      // for list
+#include <set>       // for set
+#include <fstream>   // for reading files
+#include <iomanip>   // for setw()
 #include <string>
 using namespace std;
 using namespace std::chrono;
+
 
 // function declarations
 void readData();
@@ -18,8 +19,8 @@ void sortData();
 void insertData();
 void deleteData();
 
-int main() {
 
+int main() {
     // vector, list, and set declarations
     vector<string> vec1;
     list<string> list1;
@@ -27,8 +28,45 @@ int main() {
 
     const string fileName = "codes.txt";
 
+    // header output
+    cout << setw(15) << "Operation" << setw(15) << "Vector" << setw(15) << "List" << setw(15) << "Set" << endl;
+
     // Reading Data
-    
+    auto start = high_resolution_clock::now();
+    readData();                                       // finish this code
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+
+    // Read time output
+    cout << setw(15) << "Read" << setw(15) << duration.count() << setw(15) << duration.count() << setw(15) << duration.count() << endl;
+
+    // Sort Data
+    auto start = high_resolution_clock::now();
+    sortData();                                       // finish this code
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+
+    // Sort time output
+    cout << setw(15) << "Sort" << setw(15) << duration.count() << setw(15) << duration.count() << setw(15) << duration.count() << endl;
+
+    // Insert Data
+    auto start = high_resolution_clock::now();
+    insertData();                                       // finish this code
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+
+    // Insert time output
+    cout << setw(15) << "Insert" << setw(15) << duration.count() << setw(15) << duration.count() << setw(15) << duration.count() << endl;
+
+    // Delete Data
+    auto start = high_resolution_clock::now();
+    deleteData();                                       // finish this code
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+
+    // Delete time output
+    cout << setw(15) << "Delete" << setw(15) << duration.count() << setw(15) << duration.count() << setw(15) << duration.count() << endl;
+     
 
     return 0;
 }
