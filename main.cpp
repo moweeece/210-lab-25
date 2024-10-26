a7f78f5b52135bb5375d1f2c156e924c5cf832cc
moweeece <mjespinosa990@gmail.com>
2024-10-26 12:24:50 -0700

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
+// COMSC-210 | Mauricio Espinosa | Lab 25
+// IDE Used: Visual Studio Code & Github
+
 #include <iostream>
 #include <chrono>
+#include <vector>
+#include <list>
+#include <set>
+#include <fstream>
+#include <iomanip>
 using namespace std;
+using namespace std::chrono;
+
+// function declarations
+// timing
+// reading
+// sorting
+// inserting
+// deleting
 
 int main() {
     
@@ -14,3 +30,13 @@ auto end = high_resolution_clock::now()
 auto duration = duration_cast<milliseconds>(end - start)
 duration.count() references elapsed milliseconds
 */
+
+// timing
+
+// reading
+
+// sorting
+
+// inserting
+
+// deleting
